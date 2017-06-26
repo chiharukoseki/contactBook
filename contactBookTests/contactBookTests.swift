7a749c524a195a53305980cf0fb6d701abea9f76
小関千晴 <ChiharuKoseki@Chiharu-Koseki.local>
2017-06-26 13:32:09 +0900

    contactBook

---
+++ contactBookTests/contactBookTests.swift
@@ -0,0 +1,36 @@
+//
+//  contactBookTests.swift
+//  contactBookTests
+//
+//  Created by 小関千晴 on 2017/06/26.
+//  Copyright © 2017年 小関千晴. All rights reserved.
+//
+
+import XCTest
+@testable import contactBook
+
+class contactBookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
