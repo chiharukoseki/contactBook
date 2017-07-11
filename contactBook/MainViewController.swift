6e4cce450f9f10efba220e4110f1a16ad985d549
小関 千晴 <k014c1244@it-neec.jp>
2017-07-11 11:12:21 +0900

    no message

--- contactBook/MainViewController.swift
+++ contactBook/MainViewController.swift
@@ -2,20 +2,32 @@
 //  MainViewController.swift
 //  contactBook
 //
-//  Created by 佐野浩代 on 2017/07/05.
+//  Created by 小関千晴 on 2017/07/11.
 //  Copyright © 2017年 小関千晴. All rights reserved.
 //
 
 import UIKit
+import SlideMenuControllerSwift
 
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //NavigationBarが半透明かどうか
+        navigationController?.navigationBar.isTranslucent = false
+        //NavigationBarの色を変更します
+        navigationController?.navigationBar.barTintColor = UIColor(red: 129/255, green: 212/255, blue: 78/255, alpha: 1)
+        //NavigationBarに乗っている部品の色を変更します
+        navigationController?.navigationBar.tintColor = UIColor.white
+        //バーの左側にボタンを配置します(ライブラリ特有)
+        //addLeftBarButtonWithImage(UIImage(named: "menu")!)
     }
-
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBAction func munuButton(_ sender: Any) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
